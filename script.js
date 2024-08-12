3d80e38180bd7ce9f398c92258b93a2acd69a358
AmunRaPtah <94907067+AmunRaPtah@users.noreply.github.com>
2024-08-12 12:59:46 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,353 @@
+<script>
+    const cdnBase = "https:\/\/d1vo8zfysxy97v.cloudfront.net";
+    const loggedIn = false;
+
+    var pageLocale = "en_US";
+    var defaultCountry = "US";
+    var activeCountries = [{
+        "isoCode": "BR",
+        "name": "Brazil",
+        "locale": "en_BR"
+    }, {
+        "isoCode": "CA",
+        "name": "Canada",
+        "locale": "en_CA"
+    }, {
+        "isoCode": "DO",
+        "name": "Dominican Republic",
+        "locale": "en_DO"
+    }, {
+        "isoCode": "IN",
+        "name": "India",
+        "locale": "en_IN"
+    }, {
+        "isoCode": "PH",
+        "name": "Philippines",
+        "locale": "en_PH"
+    }, {
+        "isoCode": "SG",
+        "name": "Singapore",
+        "locale": "en_SG"
+    }, {
+        "isoCode": "AE",
+        "name": "United Arab Emirates",
+        "locale": "en_AE"
+    }, {
+        "isoCode": "GB",
+        "name": "United Kingdom",
+        "locale": "en_GB"
+    }, {
+        "isoCode": "US",
+        "name": "United States",
+        "locale": "en_US"
+    }, {
+        "isoCode": "VN",
+        "name": "Vietnam",
+        "locale": "en_VN"
+    }];
+    var isProfessional = false;
+    var isAmbassador = false;
+    var isDeveloper = false;
+    var isInternational = false;
+    const restrictions = {
+        isTestingEnabled: true,
+        isShippingFree: true,
+        isProfessionEnabled: true,
+        isAllowReferrals: true,
+        isAllowRewards: true,
+        isAdvisorSessionsEnabled: true,
+        isSmsEnabled: true
+    }
+    const country = {
+        "isoCode": "US",
+        "name": "United States",
+        "locale": "en_US",
+        "defaultPricingUS": false,
+        "professionEnabled": true,
+        "regionRequired": true,
+        "shippingEnabled": true,
+        "testingEnabled": true,
+        "unitedStates": true,
+        "canada": false,
+        "shippingOriginCountryCode": "US"
+    };
+    const user = null;
+    var websiteUrl = "https:\/\/www.thorne.com";
+    const sentryEnv = "production";
+    const sentryDsn = "https:\/\/5b4dc6cc88fd4c0fb1e5fb537a077306@o4505274331430912.ingest.sentry.io\/4505274335559680";
+    const sentrySampleRate = "0.08";
+</script>
+<script src="//d1vo8zfysxy97v.cloudfront.net/js/vendor-82ad3c38c66bcaac7c15181a0dd16081.js"></script>
+<script src="//d1vo8zfysxy97v.cloudfront.net/js/global-243a5997fffb593cff6145fdc827ce36.js"></script>
+<iframe name="__uspapiLocator" style="display: none;"></iframe><iframe name="__gppLocator" style="display: none;"></iframe>
+<script src="//d1vo8zfysxy97v.cloudfront.net/js/fp-34a5f4cb6016d55f0cedc9c0f0f670b1.js"></script>
+
+<script>
+    let _notification = null;
+
+    function notification() {
+        const value = _notification;
+        _notification = null;
+        return value;
+    }
+</script>
+
+<script src="//d1vo8zfysxy97v.cloudfront.net/js/layouts/layout-app-fe66d70fa94308f94b64f0bc5abd3f02.js"></script>
+<script>
+    null
+    null
+    null
+    null
+</script>
+
+
+<script>
+    const healthNeeds = [{
+        "name": "Sharp Mind",
+        "url": "\/products\/by-health-need\/amino-acids"
+    }, {
+        "name": "Energy",
+        "url": "\/products\/by-health-need\/bone-\u0026-joint"
+    }, {
+        "name": "mood",
+        "url": "\/products\/by-health-need\/children's-health"
+    }, {
+        "name": "Relaxation and Sleep",
+        "url": "\/products\/by-health-need\/cognition-\u0026-focus"
+    }, {
+        "name": "Longevity and Vitality",
+        "url": "\/products\/by-health-need\/energy"
+    }, {
+        "name": "Inner and Outer Beauty",
+        "url": "\/products\/by-health-need\/fish-oil-\u0026-omegas"
+    }, {
+        "name": "Weight loss",
+        "url": "\/products\/by-health-need\/gut-health"
+    }, {
+        "name": "Gut health",
+        "url": "\/products\/by-health-need\/healthy-aging"
+    }, {
+        "name": "Migraine Prevention",
+        "url": "\/products\/by-health-need\/heart-\u0026-vessels"
+    }, {
+        "name": "Immunity",
+        "url": "\/products\/by-health-need\/hormone-support"
+    }, {
+        "name": "Sexual Health",
+        "url": "\/products\/by-health-need\/immune"
+    }, {
+        "name": "Workout and Athletic Performance",
+        "url": "\/products\/by-health-need\/liver-\u0026-detox"
+    }, {
+        "name": "Detox",
+        "url": "\/products\/by-health-need\/men's-health"
+    }, {
+        "name": "Holistic health",
+        "url": "\/products\/by-health-need\/metabolism"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/methylation"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/mood"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/multivitamins"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/probiotics"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/protein-powders"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/skin,-hair-\u0026-nails"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/sleep"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/sports-performance"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/stress"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/thyroid"
+    }, {
+        "name": "",
+        "url": "\/products\/by-health-need\/women's-health"
+    }];
+</script>
+
+<script src="//d1vo8zfysxy97v.cloudfront.net/js/pages/home/home-app-34a5b18bba6a47fb6590f6738db645e3.js"></script>
+
+
+
+
+
+<div id="polaris-consent-widgets"></div>
+<div style="display: none; visibility: hidden;">
+    <script>
+        (function() {
+            var b = {
+                    "@context": "http://schema.org",
+                    "@type": "Organization",
+                    name: "Thorne",
+                    url: "https://www.thorne.com",
+                    sameAs: ["https://www.facebook.com/pages/Thorne-Research/111910155537674", "https://www.instagram.com/thorneresearch", "https://twitter.com/Thorne_Research", "https://www.linkedin.com/company/thorne-research"]
+                },
+                a = document.createElement("script");
+            a.type = "application/ld+json";
+            a.innerHTML = JSON.stringify(b);
+            document.getElementsByTagName("head")[0].appendChild(a)
+        })(document);
+    </script>
+</div>
+<div style="display: none; visibility: hidden;">
+    <script>
+        (function() {
+            var b = {
+                    "@context": "http://schema.org",
+                    "@type": "WebSite",
+                    url: "https://www.thorne.com/",
+                    potentialAction: {
+                        "@type": "SearchAction",
+                        target: "https://www.thorne.com/search?q\x3d{search_term_string}",
+                        "query-input": "required name\x3dsearch_term_string"
+                    }
+                },
+                a = document.createElement("script");
+            a.type = "application/ld+json";
+            a.innerHTML = JSON.stringify(b);
+            document.getElementsByTagName("head")[0].appendChild(a)
+        })(document);
+    </script>
+</div>
+<script type="text/javascript" id="">
+    (function(a, b) {
+        var d = "pdst-capture",
+            e = "script";
+        if (!b.getElementById(d)) {
+            a.pdst = a.pdst || function() {
+                (a.pdst.q = a.pdst.q || []).push(arguments)
+            };
+            var c = b.createElement(e);
+            c.id = d;
+            c.async = 1;
+            c.src = "https://cdn.pdst.fm/ping.min.js";
+            b = b.getElementsByTagName(e)[0];
+            b.parentNode.insertBefore(c, b)
+        }
+        a.pdst("conf", {
+            key: "54d5f06c79c040b1952913db6e5cb635"
+        });
+        a.pdst("view")
+    })(window, document);
+</script>
+<script type="text/javascript" id="">
+    (function(e, f, c) {
+        var b = function() {
+            b.c(arguments)
+        };
+        b.q = [];
+        b.c = function(a) {
+            b.q.push(a)
+        };
+        var g = function() {
+            var a = c.createElement("script");
+            a.type = "text/javascript";
+            a.async = !0;
+            a.src = "https://code.upscope.io/odAuhqFvUg.js";
+            var d = c.getElementsByTagName("script")[0];
+            d.parentNode.insertBefore(a, d)
+        };
+        "function" !== typeof f && (e.Upscope = b, g())
+    })(window, window.Upscope, document);
+    Upscope("init");
+</script>
+
+<script type="text/javascript" id="">
+    Upscope("updateConnection", {
+        uniqueId: "USER UNIQUE ID",
+        identities: ["list", "of", "identities", "here"]
+    });
+</script>
+<script type="text/javascript" id="">
+    (function(a, e, b, f, g, c, d) {
+        a[b] = a[b] || function() {
+            (a[b].q = a[b].q || []).push(arguments)
+        };
+        c = e.createElement(f);
+        c.async = 1;
+        c.src = "https://www.clarity.ms/tag/" + g + "?ref\x3dgtm2";
+        d = e.getElementsByTagName(f)[0];
+        d.parentNode.insertBefore(c, d)
+    })(window, document, "clarity", "script", "j8zbo69fka");
+</script><img src="https://trkn.us/pixel/conv/ppt=23847;g=sitewide;gid=58574;ord=400346952" height="0" width="0" border="0"><img src="https://trkn.us/pixel/conv/ppt=23847;g=homepage;gid=58575;ord=2014059414" height="0" width="0" border="0">
+<script type="text/javascript" id="">
+    (function() {
+        Grin = window.Grin || (window.Grin = []);
+        var a = document.createElement("script");
+        a.type = "text/javascript";
+        a.async = !0;
+        a.src = "https://d38xvr37kwwhcm.cloudfront.net/js/grin-sdk.js";
+        var b = document.getElementsByTagName("script")[0];
+        b.parentNode.insertBefore(a, b)
+    })();
+</script>
+<script type="text/javascript" id="">
+    function loadScript() {
+        if (window.pubSuite) console.log("pubSuite snippet already included");
+        else {
+            var a = document.createElement("script");
+            a.type = "text/javascript";
+            a.src = "https://storage.googleapis.com/kochava-web-assets/ps/v1.3.0/pubsuite.js";
+            a.async = !0;
+            a.onload = function() {
+                var b = "adomni_thorne",
+                    c = "976c691e5f495b81c3dc7f60e524b451c9f85d26";
+                window.pubSuite.startWithProductId(b, c);
+                window.pubSuite.sendConversion("session_start")
+            };
+            document.head.appendChild(a)
+        }
+    }
+    loadScript();
+</script><iframe allow="join-ad-interest-group" data-tagging-id="AW-852782229" data-load-time="1723461274383" height="0" width="0" src="https://td.doubleclick.net/td/rul/852782229?random=1723461274372&cv=11&fst=1723461274372&fmt=3&bg=ffffff&guid=ON&async=1&gtm=45be4880z8810949058za201zb810949058&gcd=13v3v3v3v5&dma=0&tag_exp=0&u_w=1920&u_h=1080&url=https%3A%2F%2Fwww.thorne.com%2F&hn=www.googleadservices.com&frm=0&tiba=Supplements%20and%20Health%20Tests%20%7C%20Thorne&npa=0&us_privacy=1---&pscdl=noapi&auid=397633670.1723411238&uaa=x86&uab=64&uafvl=Not)A%253BBrand%3B99.0.0.0%7CGoogle%2520Chrome%3B127.0.6533.90%7CChromium%3B127.0.6533.90&uamb=0&uam=&uap=Windows&uapv=10.0.0&uaw=0&fledge=1" style="display: none; visibility: hidden;"></iframe><iframe allow="join-ad-interest-group" data-tagging-id="AW-852782229/ezb7CLLlrNoBEJXZ0ZYD" data-load-time="1723461274420" height="0" width="0" src="https://td.doubleclick.net/td/rul/852782229?random=1723461274392&cv=11&fst=1723461274392&fmt=3&bg=ffffff&guid=ON&async=1&gtm=45be4880z8810949058za201zb810949058&gcs=G111&gcd=13v3v3v3v5&dma=0&tag_exp=0&u_w=1920&u_h=1080&url=https%3A%2F%2Fwww.thorne.com%2F&label=ezb7CLLlrNoBEJXZ0ZYD&hn=www.googleadservices.com&frm=0&tiba=Supplements%20and%20Health%20Tests%20%7C%20Thorne&value=0&bttype=purchase&npa=0&us_privacy=1---&pscdl=noapi&auid=397633670.1723411238&uaa=x86&uab=64&uafvl=Not)A%253BBrand%3B99.0.0.0%7CGoogle%2520Chrome%3B127.0.6533.90%7CChromium%3B127.0.6533.90&uamb=0&uam=&uap=Windows&uapv=10.0.0&uaw=0&fledge=1&capi=1&ct_cookie_present=0" style="display: none; visibility: hidden;"></iframe>
+<script type="text/javascript" id="">
+    var APP_ID = "fzj5140x";
+    window.intercomSettings = {
+        app_id: APP_ID,
+        email: google_tag_manager["rm"]["10949058"](13),
+        vertical_padding: 40
+    };
+    (function() {
+        var b = window,
+            a = b.Intercom;
+        if ("function" === typeof a) a("reattach_activator"), a("update", b.intercomSettings);
+        else {
+            var e = document,
+                d = function() {
+                    d.c(arguments)
+                };
+            d.q = [];
+            d.c = function(c) {
+                d.q.push(c)
+            };
+            b.Intercom = d;
+            a = function() {
+                var c = e.createElement("script");
+                c.type = "text/javascript";
+                c.async = !0;
+                c.src = "https://widget.intercom.io/widget/" + APP_ID;
+                var f = e.getElementsByTagName("script")[0];
+                f.parentNode.insertBefore(c, f)
+            };
+            "complete" === document.readyState ? a() : b.attachEvent ? b.attachEvent("onload", a) : b.addEventListener("load",
+                a, !1)
+        }
+    })();
+</script>
+<div id="batBeacon49116520083" style="width: 0px; height: 0px; display: none; visibility: hidden;"><img id="batBeacon509514264760" width="0" height="0" alt="" src="https://bat.bing.com/action/0?ti=17203695&tm=gtm002&Ver=2&mid=d1090ee5-0932-4c54-bbda-1b86b838716e&sid=924632e0582711ef8bf569102496bbc6&vid=924665d0582711ef9b539129ecc89e5e&vids=0&msclkid=N&uach=pv%3D10.0.0&pi=918639831&lg=en-US&sw=1920&sh=1080&sc=24&tl=Supplements%20and%20Health%20Tests%20%7C%20Thorne&p=https%3A%2F%2Fwww.thorne.com%2F&r=&lt=6438&mtp=10&evt=pageLoad&sv=1&cdb=AQAA&rn=793905" style="width: 0px; height: 0px; display: none; visibility: hidden;"></div><iframe id="intercom-frame" style="position: absolute !important; opacity: 0 !important; width: 1px !important; height: 1px !important; top: 0 !important; left: 0 !important; border: none !important; display: block !important; z-index: -1 !important; pointer-events: none;" aria-hidden="true" tabindex="-1" title="Intercom"></iframe>
+<div id="upscope___remote-control-container" style="color-scheme: initial; forced-color-adjust: initial; mask: initial; math-depth: initial; position: initial; position-anchor: initial; text-size-adjust: initial; appearance: initial; color: initial; font: initial; font-palette: initial; font-synthesis: initial; inset-area: initial; text-orientation: initial; text-rendering: initial; text-spacing-trim: initial; -webkit-font-smoothing: initial; -webkit-locale: initial; -webkit-text-orientation: initial; -webkit-writing-mode: initial; writing-mode: initial; zoom: initial; accent-color: initial; place-content: initial; place-items: initial; place-self: initial; alignment-baseline: initial; anchor-name: initial; animation-composition: initial; animation: initial; app-region: initial; aspect-ratio: initial; backdrop-filter: initial; backface-visibility: initial; background: initial; background-blend-mode: initial; baseline-shift: initial; baseline-source: initial; block-size: initial; border-block: initial; border: initial; border-radius: initial; border-collapse: initial; border-end-end-radius: initial; border-end-start-radius: initial; border-inline: initial; border-start-end-radius: initial; border-start-start-radius: initial; inset: initial; box-shadow: initial; box-sizing: initial; break-after: initial; break-before: initial; break-inside: initial; buffered-rendering: initial; caption-side: initial; caret-color: initial; clear: initial; clip: initial; clip-path: initial; clip-rule: initial; color-interpolation: initial; color-interpolation-filters: initial; color-rendering: initial; columns: initial; column-fill: initial; gap: initial; column-rule: initial; column-span: initial; contain: initial; contain-intrinsic-block-size: initial; contain-intrinsic-size: initial; contain-intrinsic-inline-size: initial; container: initial; content: initial; content-visibility: initial; counter-increment: initial; counter-reset: initial; counter-set: initial; cursor: initial; cx: initial; cy: initial; d: initial; display: block; dominant-baseline: initial; empty-cells: initial; field-sizing: initial; fill: initial; fill-opacity: initial; fill-rule: initial; filter: initial; flex: initial; flex-flow: initial; float: initial; flood-color: initial; flood-opacity: initial; grid: initial; grid-area: initial; height: initial; hyphenate-character: initial; hyphenate-limit-chars: initial; hyphens: initial; image-orientation: initial; image-rendering: initial; initial-letter: initial; inline-size: initial; inset-block: initial; inset-inline: initial; isolation: initial; letter-spacing: initial; lighting-color: initial; line-break: initial; list-style: initial; margin-block: initial; margin: initial; margin-inline: initial; marker: initial; mask-type: initial; math-shift: initial; math-style: initial; max-block-size: initial; max-height: initial; max-inline-size: initial; max-width: initial; min-block-size: initial; min-height: initial; min-inline-size: initial; min-width: initial; mix-blend-mode: initial; object-fit: initial; object-position: initial; object-view-box: initial; offset: initial; opacity: initial; order: initial; orphans: initial; outline: initial; outline-offset: initial; overflow-anchor: initial; overflow-clip-margin: initial; overflow-wrap: initial; overflow: initial; overlay: initial; overscroll-behavior-block: initial; overscroll-behavior-inline: initial; overscroll-behavior: initial; padding-block: initial; padding: initial; padding-inline: initial; page: initial; page-orientation: initial; paint-order: initial; perspective: initial; perspective-origin: initial; pointer-events: initial; position-try: initial; position-visibility: initial; quotes: initial; r: initial; resize: initial; rotate: initial; ruby-position: initial; rx: initial; ry: initial; scale: initial; scroll-behavior: initial; scroll-margin-block: initial; scroll-margin: initial; scroll-margin-inline: initial; scroll-padding-block: initial; scroll-padding: initial; scroll-padding-inline: initial; scroll-snap-align: initial; scroll-snap-stop: initial; scroll-snap-type: initial; scroll-timeline: initial; scrollbar-color: initial; scrollbar-gutter: initial; scrollbar-width: initial; shape-image-threshold: initial; shape-margin: initial; shape-outside: initial; shape-rendering: initial; size: initial; speak: initial; stop-color: initial; stop-opacity: initial; stroke: initial; stroke-dasharray: initial; stroke-dashoffset: initial; stroke-linecap: initial; stroke-linejoin: initial; stroke-miterlimit: initial; stroke-opacity: initial; stroke-width: initial; tab-size: initial; table-layout: initial; text-align: initial; text-align-last: initial; text-anchor: initial; text-combine-upright: initial; text-decoration: initial; text-decoration-skip-ink: initial; text-emphasis: initial; text-emphasis-position: initial; text-indent: initial; text-overflow: initial; text-shadow: initial; text-transform: initial; text-underline-offset: initial; text-underline-position: initial; white-space: initial; timeline-scope: initial; touch-action: initial; transform: initial; transform-box: initial; transform-origin: initial; transform-style: initial; transition: initial; translate:
